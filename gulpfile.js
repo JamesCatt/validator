b5d433b2be5ef9470fc01a5c3379672774345db3
James Catt <jcatt@evergreen.ca>
2018-09-10 16:32:25 -0400

    Remove manual testing files

--- gulpfile.js
+++ gulpfile.js
@@ -16,6 +16,7 @@ gulp.task('default', () => {
         }),
         uglify(),
         rename('validator.min.js'),
+        gulp.dest('test'),
         gulp.dest('dist'),
 
     ],
--- jest-puppeteer.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  server: {
-    command: './node_modules/http-server/bin/http-server ./',
-    port: 8080,
-  },
-}
