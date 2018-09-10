1c9f04d941c98c0cadd527d6da58e5e46e272157
James Catt <jcatt@evergreen.ca>
2018-09-10 16:05:19 -0400

    Add uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,20 @@
 const gulp = require('gulp');
 const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
 
 gulp.task('default', () => {
     
-    gulp.src('src/validator.es6.js')
-        .pipe(babel({
-            presets: ['@babel/env'],
-        }))
-        .pipe(gulp.dest('dist'));
+    pump([
+
+        gulp.src('src/validator.es6.js'),
+        babel({
+            presets: ['@babel/preset-env'],
+        }),
+        uglify(),
+        gulp.dest('dist'),
+
+    ],
+    (e) => {console.log(e)});
 
 });
