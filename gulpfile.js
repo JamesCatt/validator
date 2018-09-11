3a25bea40c6e8fc13edd68db537fdf78555c4c94
James Catt <jcatt@evergreen.ca>
2018-09-11 10:57:55 -0400

    Separate jest into separate gulp task

--- gulpfile.js
+++ gulpfile.js
@@ -5,12 +5,16 @@ const uglify = require('gulp-uglify');
 const pump = require('pump');
 const rename = require('gulp-rename');
 
-gulp.task('default', () => {
+gulp.task('test', () => {
+    return gulp.src('src/validator.es6.js')
+        .pipe(jest());
+});
+
+gulp.task('default', ['test'], () => {
     
     pump([
 
         gulp.src('src/validator.es6.js'),
-        jest(),
         babel({
             presets: ['@babel/preset-env'],
         }),
